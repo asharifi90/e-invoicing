b06234e3ae8a827e77e2de0a0f4e0e940774fbc7
asharifi90 <asharifibb@gmail.com>
2026-08-22 00:41:08 +0200

    feat: kafka config and kafka domain event publisher in adapter

---
+++ src/main/java/com/einvoicing/invoice/adapter/out/messaging/KafkaDomainEventPublisher.java
@@ -0,0 +1,38 @@
+package com.einvoicing.invoice.adapter.out.messaging;
+
+import com.einvoicing.invoice.application.port.out.DomainEventPublisher;
+import com.einvoicing.invoice.event.InvoiceReceivedEvent;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Component;
+
+import static com.einvoicing.invoice.config.KafkaTopicConfig.INVOICE_RECEIVED_TOPIC;
+
+@Component
+public class KafkaDomainEventPublisher implements DomainEventPublisher {
+
+    private static final Logger log = LoggerFactory.getLogger(KafkaDomainEventPublisher.class);
+    private final KafkaTemplate<String, Object> kafkaTemplate;
+
+    public KafkaDomainEventPublisher(KafkaTemplate<String, Object> kafkaTemplate) {
+        this.kafkaTemplate = kafkaTemplate;
+    }
+
+    @Override
+    public void publish(InvoiceReceivedEvent event) {
+
+        String key = event.getEventId().toString();
+
+        log.info("publishing invoice received event for invoiceId: {}", key);
+        kafkaTemplate.send(INVOICE_RECEIVED_TOPIC, key, event)
+                .whenComplete((result, error) -> {
+                    if (error != null) {
+                        log.error("error while publishing invoice received event for invoiceId: {}", key);
+                    } else {
+                        log.debug("successfully published invoice received event for invoiceId: {}", key);
+                    }
+
+                });
+    }
+}
---
+++ src/main/java/com/einvoicing/invoice/config/KafkaTopicConfig.java
@@ -0,0 +1,20 @@
+package com.einvoicing.invoice.config;
+
+import org.apache.kafka.clients.admin.NewTopic;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.config.TopicBuilder;
+
+@Configuration
+public class KafkaTopicConfig {
+
+    public static final String INVOICE_RECEIVED_TOPIC = "invoice.received";
+
+    @Bean
+    public NewTopic invoiceReceivedTopic() {
+        return TopicBuilder.name(INVOICE_RECEIVED_TOPIC)
+                .partitions(3)
+                .replicas(1)
+                .build();
+    }
+}
