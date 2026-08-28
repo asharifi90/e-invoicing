f01121d477a86cbf6f91dcdb37f3bd9366b87171
asharifi90 <asharifibb@gmail.com>
2026-08-28 16:27:18 +0200

    feat: validation service main spring application

---
+++ validation-invoice/src/main/java/com/einvoicing/validation/ValidationInvoiceApplication.java
@@ -0,0 +1,12 @@
+package com.einvoicing.validation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ValidationInvoiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ValidationInvoiceApplication.class, args);
+    }
+}
