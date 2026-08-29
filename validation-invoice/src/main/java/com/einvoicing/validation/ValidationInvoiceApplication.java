9cad071c464057d69a5f7477fffd168dafe4508c
asharifi90 <asharifibb@gmail.com>
2026-08-29 20:43:57 +0200

    feat: add enable kafka annotation

--- validation-invoice/src/main/java/com/einvoicing/validation/ValidationInvoiceApplication.java
+++ validation-invoice/src/main/java/com/einvoicing/validation/ValidationInvoiceApplication.java
@@ -2,8 +2,10 @@ package com.einvoicing.validation;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.kafka.annotation.EnableKafka;
 
 @SpringBootApplication
+@EnableKafka
 public class ValidationInvoiceApplication {
 
     public static void main(String[] args) {
